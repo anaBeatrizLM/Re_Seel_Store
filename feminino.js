214dcff6b7a92c8ddb473d88357ecf934d1a8d8a
anaBeatrizLM <101081223+anaBeatrizLM@users.noreply.github.com>
2022-03-18 21:34:22 -0300

    inserindo os arquivos

---
+++ feminino.js
@@ -0,0 +1,15 @@
+function show_categorias() {
+    document.getElementById("menu_categorias").style.visibility = "visible";
+}
+
+function hide_categorias() {
+    document.getElementById("menu_categorias").style.visibility = "hidden";
+}
+
+function show_vestuario() {
+    document.getElementById("menu_vestuario").style.visibility = "visible";
+}
+
+function hide_vestuario() {
+    document.getElementById("menu_vestuario").style.visibility = "hidden";
+}
